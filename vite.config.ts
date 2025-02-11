1aab8f7d5cd8ca8d729d0d544e568bfd123a4813
Surjit Kumar <128356922+Surjit27@users.noreply.github.com>
2025-02-12 01:08:37 +0530

    Updated vite.config.ts and fixed GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Gyat/', // <-- Add your repository name as the base path
   plugins: [react()],
   build: {
     rollupOptions: {
