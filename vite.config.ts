8e9932a9dd91be0fe4aac02340de74b772efc9c6
Surjit Kumar <128356922+Surjit27@users.noreply.github.com>
2025-02-12 01:23:03 +0530

    Fix paths for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Gyat/', // 👈 Ensure this matches your GitHub repo name
+  base: '/Gyat/',  // 👈 Replace with your GitHub repo name
   plugins: [react()],
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: './index.html',
-    },
   },
 });
